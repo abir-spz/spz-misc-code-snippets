9e336beaf7c1710a5b7f3297becd52f0424b1eda
Abir Maiti <abir@spiralyze.com>
2026-05-20 10:48:11 +0530

    VWO AQA targets with regex

---
+++ code-snippets/vwo-regex-page-targeting-for-aqa.js
@@ -0,0 +1,14 @@
+/*
+
+VWO AQA Target URLS (Regex) (URL matches Regex)
+
+^https:\/\/www\.matik\.io\/request-demo-streamline-presentation-personalization?.*\bspz=2003\b.*$
+^https:\/\/www\.matik\.io\/request-demo--automate-your-presentations-boost-team-efficiency?.*\bspz=2003\b.*$
+^https:\/\/www\.matik\.io\/request-demo-automate-your-data-driven-content-with-ai?.*\bspz=2003\b.*$
+
+
+If any test of same series is running live then exclude using query params
+
+Exclude pages where - URL contains - spz=2003
+
+*/
