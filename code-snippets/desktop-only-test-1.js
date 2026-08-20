a2cd25751a3895d77625a1ff06dda5a19e346a37
Abir Maiti <abir@spiralyze.com>
2026-08-20 20:03:35 +0530

    update

--- code-snippets/desktop-only-test-1.js
+++ code-snippets/desktop-only-test-1.js
@@ -1,3 +1,4 @@
+//=========================== METHOD 1
 // This following code is for desktop only test
 const DESKTOP_BREAKPOINT = 1025;
 
@@ -20,3 +21,29 @@ window.addEventListener('resize', () => {
 
 // Don't run the test below 1025px
 if (!isDesktop()) return;
+
+// Rest of A/B test code
+
+
+
+
+//=========================== METHOD 2
+// This following code is for desktop only test
+const desktopQuery = window.matchMedia('(min-width: 1025px)');
+
+if (!desktopQuery.matches) {
+	return;
+}
+
+function handleViewportChange(event) {
+	if (event.matches) {
+		window.location.reload();
+	} 
+	else {
+		window.location.reload();
+	}
+}
+
+desktopQuery.addEventListener('change', handleViewportChange);
+
+// Rest of A/B test code
